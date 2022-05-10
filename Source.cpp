4cccf8469666a30fce0e106436f61e03d5d44d63
illy skalikxyb <pagany10@gmail.com>
2022-05-10 12:35:45 +0200

    Done exercise #4

--- Source.cpp
+++ Source.cpp
@@ -7,6 +7,51 @@ using namespace std;
 
 int main()
 {
-
+	ifstream file;
+	ofstream file2;
+	string strfile;
+	int length = 0, number = 0, lengthNum = 10;
+	int* countarray = new int[lengthNum];
+	for (int i = 0; i < lengthNum; i++)
+	{
+		countarray[i] = 0;
+	}
+	file.open("file.txt", ios::in);
+	file2.open("file2.txt", ios::out);
+	if (!file.is_open())
+	{
+		cout << "Not found";
+	}
+	else
+	{
+		while (getline(file, strfile))
+		{
+			length = strfile.length();
+			for (int i = 0; i < length; i++)
+			{
+				if (isdigit(strfile[i]) != 0)
+				{
+					number = strfile[i] - '0';
+				}
+				for (int j = 0; j < lengthNum; j++)
+				{
+					if (number == j)
+					{
+						countarray[j] += 1;
+					}
+				}
+			}
+		}
+		number = 0;
+		for (int i = 0; i < lengthNum; i++)
+		{
+			file2 << "Number of digits '" << i << "' = " << countarray[i] << endl;
+			number += countarray[i];
+		}
+		file2 << "Total digits: " << number;
+	}
+	file2.close();
+	file.close();
+	delete[] countarray;
 	return 0;
 }
