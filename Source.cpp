b57cfec8ff2a7d5e27e0a24d81d369d0615f8241
illy skalikxyb <pagany10@gmail.com>
2022-05-10 11:41:40 +0200

    Done exercise #3

--- Source.cpp
+++ Source.cpp
@@ -7,6 +7,50 @@ using namespace std;
 
 int main()
 {
-
+	ifstream file;
+	ofstream file2;
+	string strfile, strABC="abcdefghijklmnopqrstuvwxyz";
+	char symbol;
+	int length = 0, lengthArr = strABC.length();
+	int* countarray = new int[lengthArr];
+	for (int i = 0; i < lengthArr; i++)
+	{
+		countarray[i] = 0;
+	}
+	file.open("file.txt", ios::in);
+	file2.open("file2.txt", ios::out);
+	if (!file.is_open())
+	{
+		cout << "Not found";
+	}
+	else
+	{
+		while (getline(file, strfile))
+		{
+			length = strfile.length();
+			for (int i = 0; i < length; i++)
+			{
+				symbol = strfile[i];
+				if (!islower(symbol))
+				{
+					symbol = tolower(symbol);
+				}
+				for (int j = 0; j < lengthArr; j++)
+				{
+					if (symbol == strABC[j])
+					{
+						countarray[j] += 1;
+					}
+				}
+			}
+		}
+		for (int i = 0; i < lengthArr; i++)
+		{
+			file2 << "Number of letter '" << strABC[i] << "' = " << countarray[i] << endl;
+		}
+	}
+	file2.close();
+	file.close();
+	delete[] countarray;
 	return 0;
 }
