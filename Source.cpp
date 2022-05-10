67f1f1f1d5e201e92110ab22ce908d376f94a13d
illy skalikxyb <pagany10@gmail.com>
2022-05-10 15:15:55 +0200

    commit position #1

--- Source.cpp
+++ Source.cpp
@@ -7,6 +7,32 @@ using namespace std;
 
 int main()
 {
-
+	ifstream file;
+	ofstream file2;
+	string strfile, strfile2;
+	int countr = 0, length = 0;
+	file.open("file.txt", ios::in);
+	file2.open("file2.txt", ios::out);
+	if (!file.is_open())
+	{
+		cout << "Not found";
+	}
+	else
+	{
+		while (getline(file, strfile))
+		{
+			char* pch = strtok((char*)strfile.c_str(), ".");
+			while (pch != NULL)
+			{
+				if (islower(pch[0]))
+				{
+					pch[0] = toupper(pch[0]);
+				}
+				file2 << pch << ".";
+				pch = strtok(NULL, ".");
+			}
+			file2 << endl;
+		}
+	}
 	return 0;
 }
