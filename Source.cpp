1d034600b93a4e7bbf45b1b286598aabb4457c3d
illy skalikxyb <pagany10@gmail.com>
2022-05-10 19:08:36 +0200

    Done exercise #1

--- Source.cpp
+++ Source.cpp
@@ -7,21 +7,67 @@ using namespace std;
 
 int main()
 {
-	string strfile,replaceWord, replacementWord;
-	ifstream file("file.txt",ios::in);
-	cout << "Enter replacement word: ";
-	cin >> replacementWord;
-	cout << "Replace the word with: ";
-	cin >> replaceWord;
+	string strfile, givenword, word;
+	fstream file;
+	file.open("file.txt", ios::in);
+	cout << "Enter word: ";
+	cin >> givenword;
+	cout << "Enter word: ";
+	cin >> word;
 	if (!file.is_open())
 	{
-		cout << "Invalid" << endl;
+		cout << "Not found" << endl;
 	}
 	else
 	{
-		while (getline(file, strfile))
+		string buf, begin, end;
+		while (getline(file, buf))
 		{
-			
+			strfile += buf;
+			strfile += "\n";
+		}
+		file.close();
+		int pos;
+		cout << strfile << endl;
+		
+		do
+		{
+			buf.clear();
+			pos = strfile.find(givenword);
+			if (pos == -1)
+			{
+				break;
+			}
+			else
+			{
+				
+				for (size_t i = 0; i < strfile.length(); i++)
+				{
+					if (i < pos)
+					{
+						begin+=strfile[i];
+					}
+					else if (i >= pos + givenword.size())
+					{
+						end+=strfile[i];
+					}
+				}
+				buf = begin + word + end;
+				strfile = buf;
+				begin.clear();
+				end.clear();
+				cout << buf << endl;
+			}
+		} while (true);
+		file.open("file.txt", ios::out | ios::ate);
+		if (!file.is_open())
+		{
+			cout << "Not found" << endl;
+		}
+		else
+		{
+			file << strfile << endl;
+			file.close();
 		}
 	}
 	return 0;
